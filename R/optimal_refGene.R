3b2a6c2051cf39269f7563f1b9aa5bc0c9db3f6e
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2017-09-25 13:31:08 -0500

    improve function to get optimal reference gene: + improve handling of sleuth file (either old 'load' method or new 'readRDS' method) + include alternative ways of calculating variance: -- expand default TPM to the other units (estimated counts and scaled reads per base) -- old method of taking mean and standard deviation directly from normalized table ("obs_norm" with "spread_abundance_by") -- combined variance of "var_obs" and "tech_var" from sleuth results table -- just variance of "var_obs".

--- R/optimal_refGene.R
+++ R/optimal_refGene.R
@@ -27,26 +27,62 @@ reduceToOneCandidate <- function(ids, means) {
 # candidate #2: the transcript with the least coefficient of variation
 #   across all transcripts in the highest 25% of mean expression
 # candidate #3: the transcript with the least coefficient of variation
-#   across all transcripts with a mean abundance of 10 TPM (top ~5-10%)
+#   across all transcripts with a mean abundance of 10 TPM or
+#   5000 counts/reads per base (usually top ~5-10%)
 # if there are ties, it tries to prioritize the transcript with the highest
 # abundance, and otherwise takes the first one.
-get_least_var_targets <- function(sleuth_file) {
-  load(sleuth_file)
-  table <- sleuth:::spread_abundance_by(sleuth.obj$obs_norm, "tpm")
+get_least_var_targets <- function(sleuth.obj = NULL, which_var = "tpm") {
+  if (is.character(sleuth.obj)) {
+    tryCatch(sleuth.obj <- sleuth::sleuth_load(sleuth.obj),
+             error = function(e) load(sleuth.obj))
+  } else {
+    stopifnot(is(sleuth.obj, "sleuth"))
+  }
+
+  which_var <- match.arg(which_var, c("tpm", "est_counts", "scaled_reads_per_base"))
+
+  table <- sleuth:::spread_abundance_by(sleuth.obj$obs_norm, which_var)
+  table_cov <- apply(table, 1, function(x) sd(x) / mean(x))
   table <- table[which(rownames(table) %in% sleuth.obj$filter_df$target_id), ]
-  cov <- apply(table, 1, function(x) sd(x) / mean(x))
+
+  results <- sleuth::sleuth_results(sleuth.obj, names(sleuth.obj$tests$wt[[1]])[1])
+  results <- results[order(results$target_id), ]
+
+  nsamples <- ncol(table)
+  variance <- nsamples * (results$var_obs + results$tech_var) /
+    (2 * nsamples - 2)
+
+  cov <- sqrt(variance) / results$mean_obs
+  est_cov <- sqrt(results$var_obs) / results$mean_obs
+
+  names(cov) <- names(est_cov) <- results$target_id
+  cov <- cov[which(names(cov) %in% rownames(table))]
+  est_cov <- est_cov[which(names(est_cov) %in% rownames(table))]
+  table_cov <- table_cov[which(names(table_cov) %in% rownames(table))]
+
   means <- apply(table, 1, mean)
+
   small_id <- names(which(cov == min(cov, na.rm=T)))
-  print(small_id)
+  small_est_id <- names(which(est_cov == min(est_cov, na.rm=T)))
+  small_table_id <- names(which(table_cov == min(table_cov, na.rm=T)))
+
   topquart <- summary(means)[5]
   topquart_id <- names(which(cov == min(cov[which(means>=topquart)])))
-  print(topquart_id)
-  
-  highexp_id <- names(which(cov == min(cov[which(means>=10)])))
-  print(highexp_id)
-  
+  topquart_est_id <- names(which(est_cov == min(est_cov[which(means>=topquart)])))
+  topquart_table_id <- names(which(table_cov == min(table_cov[which(means>=topquart)])))
+
+  threshold <- ifelse(which_var == "tpm", 10, 5000)
+  highexp_id <- names(which(cov == min(cov[which(means>=threshold)])))
+  highexp_est_id <- names(which(est_cov == min(est_cov[which(means>=threshold)])))
+  highexp_table_id <- names(which(table_cov == min(table_cov[which(means>=threshold)])))
+
   small_id <- reduceToOneCandidate(small_id, means)
   topquart_id <- reduceToOneCandidate(topquart_id, means)
   highexp_id <- reduceToOneCandidate(highexp_id, means)
-  list(small_id, topquart_id, highexp_id)
+
+  ids <- c(small_id, topquart_id, highexp_id)
+  est_ids <- c(small_est_id, topquart_est_id, highexp_est_id)
+  table_ids <- c(small_table_id, topquart_table_id, highexp_table_id)
+  names(ids) <- names(est_ids) <- names(table_ids) <- c("small_id", "topquartile_id", "highTPM_id")
+  list(ids = ids, est_ids = est_ids, table_ids = table_ids)
 }
