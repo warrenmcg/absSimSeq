dfdd44a4afe3542600d255c9a874aa6fee5a67e2
warrenmcg <warren-mcgee@fsm.northwestern.edu>
2018-07-16 14:24:22 -0500

    change how failed dispersions are interpolated to avoid errors generated when running polyester

--- R/calculate_sizes.R
+++ R/calculate_sizes.R
@@ -29,14 +29,19 @@ calculate_sizes <- function(counts = NULL, s2c, design, reads_pt = NULL,
   disp_filt <- ifelse(is.na(disp_filt), FALSE, disp_filt)
   if (is.null(dim(dispersions))) {
     med_dispersion <- median(dispersions[disp_filt])
-    dispersions <- ifelse(disp_filt, dispersions, med_dispersion)
+    dispersions[!disp_filt] <- med_dispersion
   } else {
     med_dispersions <- sapply(1:2, function(i) {
       median(dispersions[, i][disp_filt[, i]])
     })
     dispersions <- sapply(1:2, function(i) {
-      ifelse(disp_filt[, i], dispersions[, i], med_dispersions[i])
+      med_disp <- med_dispersions[i]
+      disps <- dispersions[, i]
+      disps[!disp_filt[, i]] <- med_disp
+      disps
     })
+    dispersions <- t(dispersions)
+    stopifnot(dim(dispersions) == dim(basemeans))
   }  
   # size (i.e. r) = 1 / dispersion
   sizes <- 1 / dispersions
